4f1470a24e18b62eceb6c2ea4e513fdd360f4984
Peter Hall <p.g.l.hall+github@gmail.com>
2020-04-25 22:35:51 +0100

    Redux additions

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import { applyMiddleware, compose, createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
+
+import rootReducer from "./reducers/rootReducer/rootReducer";
+import rootSaga from "./sagas/rootSaga/rootSaga";
+
+const middlewares = [createSagaMiddleware()];
+
+// Add redux devtools if in a browser environment and if installed
+// For TypeScript we would be using redux-devtools-extension instead
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
