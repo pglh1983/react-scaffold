2ec4930ac23e870155296e2d4fc6283478c452b6
Peter Hall <p.g.l.hall+github@gmail.com>
2020-04-25 22:57:33 +0100

    Integrate store into app

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,9 @@
  */
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from './store/index';
 
 // Hot module replacement - parcel, dev mode
 if (module.hot) {
@@ -10,7 +13,9 @@ if (module.hot) {
 }
 
 const MainApp = () => (
-  <h1>Hello React!</h1>
+  <Provider store={store}>
+    <h1>Hello React!</h1>
+  </Provider>
 );
 // render the app
 render(<MainApp />, document.getElementById('app'));
