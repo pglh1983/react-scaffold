1ea6974bdd00993bd7e1e8d05b6d78fd68f609f9
Peter Hall <p.g.l.hall+github@gmail.com>
2020-04-25 23:00:30 +0100

    Remove cruft

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { addDecorator, configure } from '@storybook/react';
+import { configure } from '@storybook/react';
 import requireContext from 'require-context.macro';
 
 // automatically import all files ending in *.stories.js
@@ -9,12 +8,4 @@ function loadStories() {
   req.keys().forEach((filename) => req(filename));
 }
 
-const globalDependencies = story => (
-  <>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600&display=swap" rel="stylesheet"/>
-    {story()}
-  </>
-);
-
-addDecorator(globalDependencies)
 configure(loadStories, module);
